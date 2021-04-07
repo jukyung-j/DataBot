7a3dfa56dca57c7c46dd641034651c282f736140
jukyung-j <wjdwnrud123@naver.com>
2021-04-08 02:06:53 +0900

    checkbox

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainWindow(QMainWindow):
         fileName, _ = QFileDialog.getOpenFileName(self, "데이터 파일 열기", \
                                                   "", "All Files(*);;CSV data files(*.csv)", options=options)
         if fileName:
+            global df
             df = pd.read_csv(fileName)
             file_path = os.path.splitext(fileName)[0]
             file_name = file_path.split('/')[-1]
@@ -26,7 +27,25 @@ class MainWindow(QMainWindow):
             self.ui.relation.setText(file_name)
             self.ui.attributes.setText(str(df.shape[1]))
             self.ui.instances.setText(str(len(df)))
+            self.ui.tableWidget.setRowCount(df.shape[1])
 
+            self.ui.check_list = []
+            for i in range(df.shape[1]):
+                self.ui.checkBox = QCheckBox(self.ui.groupBox_3)
+                self.ui.check_list.append(self.ui.checkBox)
+                self.ui.check_list[i].setText(df.columns[i])
+                self.ui.check_list[i].setChecked(True)
+                self.ui.tableWidget.setCellWidget(i, 0, self.ui.check_list[i])
+                #self.ui.tableWidget.setItem(i,0,QTableWidgetItem())
+
+            for i in range(df.shape[1]):
+                self.ui.check_list[i].stateChanged.connect(self.check)
+
+
+    def check(self, state):
+        for i in range(df.shape[1]):
+            if self.ui.check_list[i].isChecked() == False:
+                print(df.columns[i])
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
