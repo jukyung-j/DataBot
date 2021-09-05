bae2b6a9b6d11ad486c95d65d14e386e61d25064
jukyung-j <68947314+jukyung-j@users.noreply.github.com>
2021-09-05 22:05:51 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from PyQt5.QtCore import QFile,QIODevice
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-import pandas as pd #데이터 프레임과 시리즈를 사용하기 쉽게
+import pandas as pd # 데이터 프레임과 시리즈를 사용하기 쉽게
 import numpy as np  # 수학적 연산을 쉽게
 
 import matplotlib.pyplot as plt  # 차트그리기
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas    # 그래프그리기
 
 from pandas.api.types import is_numeric_dtype   # 선택된 속성이 숫자형인지 알아내기 위해
 from pandas.api.types import is_string_dtype    # 선택된 속성이 문자형인지 알아내기 위해
 from sklearn.model_selection import train_test_split    # train데이터와 test데이터 분히
 from sklearn.preprocessing import LabelEncoder  # 문자형데이터를 숫자로 바꾸기 위해
 from sklearn.preprocessing import StandardScaler    # 데이터 표준화
+from sklearn.impute import SimpleImputer    # 결측치 처리방법
 
 from sklearn.metrics import accuracy_score, recall_score, precision_score   # 분류모델에서 성능평가 지표인 정확도, 재현율, 정밀도 계산
 from sklearn.metrics import mean_squared_error  # 회귀모델에서 성능평가 지표인 MSE 계산
@@ -57,8 +58,10 @@ class MainWindow(QMainWindow, form_class):
             # 초기화
             self.list.clear()
             self.comboBox.clear()
+            self.method.clear()
             self.result.clear()
             self.comboBox.blockSignals(True) # 파일 다시 열었을 때 충돌 해결
+            self.method.blockSignals(True)
 
             for attr in df.columns: # 변수들 combobox에 추가
                 self.comboBox.addItem(attr)
@@ -84,12 +87,23 @@ class MainWindow(QMainWindow, form_class):
 
             x_train, x_test = X_train, X_test
 
+            # 데이터 전처리
+            column_length = df.shape[1]
+            self.miss.setRowCount(column_length)
+            for i in range(column_length):
+                self.miss.setItem(i, 0, QTableWidgetItem(str(df.columns[i])))
+                self.miss.setItem(i, 1, QTableWidgetItem(str(df.isnull().sum()[i])))
+            self.method.addItem("delete")
+            self.method.addItem("mean")
+            self.method.addItem("median")
+            self.method.addItem("most_frequent")
+
             # 데이터 표준화(StandardScaler)
             # Standardization 평균 0 / 분산 1
-            # scaler = StandardScaler()
-            # scaler.fit(X_train)
-            # X_train = scaler.transform(X_train)
-            # X_test = scaler.transform(X_test)
+            scaler = StandardScaler()
+            scaler.fit(X_train)
+            X_train = scaler.transform(X_train)
+            X_test = scaler.transform(X_test)
 
 
     def list_click(self):   # 속성들을 클릭하면
@@ -129,6 +143,26 @@ class MainWindow(QMainWindow, form_class):
 
         self.canvas.draw()
 
+    # 데이터전처리 방법
+    def method_apply(self):
+        global df
+        if self.method.currentText() == 'delete':   # 결측치 제거
+            df = df.dropna()
+
+        elif self.method.currentText() == 'mean':   # 평균값
+            imputer = SimpleImputer(strategy="mean")
+            df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+        elif self.method.currentText() == 'median':  # 중간값
+            imputer = SimpleImputer(strategy="median")
+            df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+        elif self.method.currentText() == 'most_frequent':   # 최빈값
+            imputer = SimpleImputer(strategy="most_frequent")
+            df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+
+
 
     def on_select(self):    #종속변수 정하기
         result = self.comboBox.currentText()
@@ -162,7 +196,6 @@ class MainWindow(QMainWindow, form_class):
         self.result.setRowCount(6)
         self.result.verticalHeader().setVisible(False)
         count = -1
-        
         if self.knn.isChecked() == True: # Knn 알고리즘
             count += 1
             
@@ -228,7 +261,8 @@ class MainWindow(QMainWindow, form_class):
             self.result.setItem(count, 1, QTableWidgetItem(str(round(accuracy_score(y_test, y_predict), 5))))
             self.result.setItem(count, 2, QTableWidgetItem(str(round(precision_score(y_test, y_predict, average='weighted'), 5))))
             self.result.setItem(count, 3, QTableWidgetItem(str(round(recall_score(y_test, y_predict, average='weighted'), 5))))
-       
+    def closeEvent(self, event):
+        self.deleteLater()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
